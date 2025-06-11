26b63ee315a391c5af6088f854d19eb0672689ac
ice-lfernandes <lucas.rj.fernandes@gmail.com>
2025-06-11 18:25:31 -0300

    vault provider usage example

---
+++ spring-secret-starter-vault-example/src/main/java/br/com/ldf/medium/spring_secret_starter_vault_example/SpringSecretStarterVaultExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.ldf.medium.spring_secret_starter_vault_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecretStarterVaultExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecretStarterVaultExampleApplication.class, args);
+	}
+
+}
---
+++ spring-secret-starter-vault-example/src/test/java/br/com/ldf/medium/spring_secret_starter_vault_example/SpringSecretStarterVaultExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ldf.medium.spring_secret_starter_vault_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecretStarterVaultExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
