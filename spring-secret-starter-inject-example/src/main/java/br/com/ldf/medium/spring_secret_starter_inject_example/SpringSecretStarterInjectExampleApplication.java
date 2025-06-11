921a886964feaf017f7036f0624c8d02744bef1c
ice-lfernandes <lucas.rj.fernandes@gmail.com>
2025-06-11 19:56:44 -0300

    initial commit

---
+++ spring-secret-starter-inject-example/src/main/java/br/com/ldf/medium/spring_secret_starter_inject_example/SpringSecretStarterInjectExampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.ldf.medium.spring_secret_starter_inject_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecretStarterInjectExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecretStarterInjectExampleApplication.class, args);
+	}
+
+}
