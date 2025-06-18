722d5db33e5a7a302f5e9859b2a0ddd2113f201d
lucas_fernandes <lucas.rj.fernandes@gmail.com>
2025-06-18 16:09:53 -0300

    update

--- spring-secret-starter-inject-example/src/main/java/br/com/ldf/medium/spring_secret_starter_inject_example/ExampleInjectFieldSecretValue.java
+++ spring-secret-starter-inject-example/src/main/java/br/com/ldf/medium/spring_secret_starter_inject_example/ExampleInjectFieldSecretValue.java
@@ -9,25 +9,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExampleInjectFieldSecretValue implements CommandLineRunner {
 
-    public record Credential(
-        String username,
-        String password
-    ) {}
-
     @SecretValue(value = "${h2.username}")
     private String username;
 
     @SecretValue(value = "${h2.password}")
     private String password;
 
-//    @SecretValue(value = "${h2.credential}", type = Credential.class)
-//    private Credential credential;
-
     @Override
     public void run(String... args) throws Exception {
         log.info("h2 username: {}", username);
         log.info("h2 password: {}", password);
-
-//        log.info("credential: {}", credential);
     }
 }
--- spring-secret-starter-inject-example/src/main/java/br/com/ldf/medium/spring_secret_starter_inject_example/persistence/PersonRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.ldf.medium.spring_secret_starter_inject_example.persistence;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-}
